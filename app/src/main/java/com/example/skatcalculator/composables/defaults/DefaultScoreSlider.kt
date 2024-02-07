c151a9d6b94c02a4fb0a0222ddf668dca3181ad1
stlcschu <luca.schuhmacher@student.hs-anhalt.de>
2024-02-07 01:07:40 +0100

    [IMPROVED] DefaultScoreSlider
    * Added animations to the numbers

--- app/src/main/java/com/example/skatcalculator/composables/defaults/DefaultScoreSlider.kt
+++ app/src/main/java/com/example/skatcalculator/composables/defaults/DefaultScoreSlider.kt
@@ -1,5 +1,18 @@
 package com.example.skatcalculator.composables.defaults
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.animation.with
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,12 +23,27 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.skatcalculator.R
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun DefaultScoreSlider(
     maxValue: Float = 120f,
@@ -24,17 +52,24 @@ fun DefaultScoreSlider(
     onClickLeft: (Float) -> Unit,
     onClickRight: (Float) -> Unit,
     onValueChanged: (Float) ->  Unit
-
 ) {
+
+    var lastValue by remember { mutableFloatStateOf(currentValue) }
+
     Row(
         modifier = Modifier
             .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Column(
             modifier = Modifier
                 .clickable {
-                    onClickLeft(if (currentValue - 1f < 0) currentValue else currentValue - 1f)
+                    lastValue = currentValue
+                    onClickLeft(
+                        if (currentValue + 1f > 120) currentValue
+                        else currentValue + 1f
+                    )
                 }
                 .width(60.dp)
         ) {
@@ -42,20 +77,41 @@ fun DefaultScoreSlider(
                 text = playerName,
                 modifier = Modifier
                     .fillMaxWidth(),
-                textAlign = TextAlign.Center,
+                textAlign = TextAlign.Right,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
-            Text(
-                text = currentValue.roundToInt().toString(),
-                modifier = Modifier
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Right
-            )
+            AnimatedContent(
+                targetState = currentValue,
+                transitionSpec = {
+                                 if (lastValue > currentValue) {
+                                     (slideInHorizontally { _ -> -0 } + fadeIn())
+                                         .togetherWith(
+                                             slideOutHorizontally { width -> width } + fadeOut()
+                                         )
+                                 } else {
+                                     (slideInHorizontally { width -> width } + fadeIn())
+                                         .togetherWith(
+                                             slideOutHorizontally { _ -> -0 } + fadeOut()
+                                         )
+                                 }.using(
+                                     SizeTransform(clip = false)
+                                 )
+                },
+                label = ""
+            ) { target ->
+                Text(
+                    text = "${target.roundToInt()}",
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Right,
+                )
+            }
         }
         Slider(
             value = currentValue,
             onValueChange = {
+                lastValue = currentValue
                 onValueChanged(it)
             },
             valueRange = 0f..maxValue,
@@ -67,8 +123,10 @@ fun DefaultScoreSlider(
         Column(
             modifier = Modifier
                 .clickable {
+                    lastValue = currentValue
                     onClickRight(
-                        if (currentValue + 1f > 120) maxValue else currentValue + 1f
+                        if (currentValue - 1f < 0) maxValue
+                        else currentValue - 1f
                     )
                 }
                 .width(60.dp)
@@ -77,14 +135,53 @@ fun DefaultScoreSlider(
                 text = "Others",
                 modifier = Modifier
                     .fillMaxWidth(),
-                textAlign = TextAlign.Center,
+                textAlign = TextAlign.Left,
             )
-            Text(
-                text = (120 - currentValue.roundToInt()).toString(),
-                modifier = Modifier
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Left
-            )
+            AnimatedContent(
+                targetState = currentValue,
+                transitionSpec = {
+                    if (lastValue > currentValue) {
+                        (slideInHorizontally { width -> -width } + fadeIn())
+                            .togetherWith(
+                                slideOutHorizontally { _ -> 0 } + fadeOut()
+                            )
+                    } else {
+                        (slideInHorizontally { _ -> 0 } + fadeIn())
+                            .togetherWith(
+                                slideOutHorizontally { width -> -width } + fadeOut()
+                            )
+                    }.using(
+                        SizeTransform(clip = false)
+                    )
+                },
+                label = ""
+            ) { target ->
+                Text(
+                    text = "${120 - target.roundToInt()}",
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Left,
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewDefaultScoreSlider() {
+    var score by remember { mutableFloatStateOf(60f)}
+    DefaultScoreSlider(
+        playerName = "Florianen",
+        currentValue = score,
+        onClickLeft = {
+                      score = it
+        },
+        onClickRight = {
+                       score = it
+        },
+        onValueChanged = {
+            score = it
+        }
+    )
 }
